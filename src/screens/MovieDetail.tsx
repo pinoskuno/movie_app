a69a9a626c860a24e829bcd7fa2892f71200e718
Rahmat Ramadhan <rakarr15@gmail.com>
2024-06-20 13:15:57 +0700

    home_screen_navigation
    home_screen_navigation tombol home

---
+++ src/screens/MovieDetail.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function MovieDetail({ navigation }: any): JSX.Element {
+  return (
+    <View>
+      <Text>Movie Detail</Text>
+    </View>
+  )
+}
